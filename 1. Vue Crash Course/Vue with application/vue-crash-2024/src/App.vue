d261f4c2b45b1de82127966a661d335163d04d7b
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2026-01-19 18:51:51 +1000

    0. Vue - Setup TailwindCSS

--- 1. Vue Crash Course/Vue with application/vue-crash-2024/src/App.vue
+++ 1. Vue Crash Course/Vue with application/vue-crash-2024/src/App.vue
@@ -1,71 +1,11 @@
 <script setup>
-import {ref, onMounted} from 'vue';
 
-const name = ref('John Doe');
-const status = ref('active');
-const tasks = ref(['Task One', 'Task Two', 'Task Three']);
-const newTask = ref('');
-
-const toggleStatus = () => {
-  if (status.value === 'active'){
-    status.value = 'pending';
-  } else if (status.value === 'pending'){
-    status.value = 'inactive';
-  } else {
-    status.value = 'active';
-  }
-};
-
-const addTask = () => {
-  if (newTask.value.trim() != '') {
-    tasks.value.push(newTask.value);
-    newTask.value = '';
-  }
-};
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-};
-
-onMounted(async() => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-    const data = await response.json();
-    tasks.value = data.map(task => task.title);
-  } catch (error) {
-    console.log('Error fetching tasks');
-  }
-});
 
 </script>
 
 <template>
-  <h1>{{name}}</h1>
+  <h1 class="text-2xl">Vue Jobs!</h1>
 
-  <br/>
-  <p v-if="status==='active'">User is active</p>
-  <p v-else-if="status==='pending'">User is pending</p>
-  <p v-else="status">User is inactive</p>
-
-  <br/>
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task </label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask"></input>
-    <button type="submit">Submit</button>
-  </form>
-
-  <br/>
-  <h3>Tasks: </h3> <br/>
-  <ul>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span> {{task}} </span>
-      <button @click="deleteTask(index)">x</button>
-    </li>
-  </ul>
-  
-  <br/>
-  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-  <button @click="toggleStatus">Change Status</button>
 
 </template>
 
