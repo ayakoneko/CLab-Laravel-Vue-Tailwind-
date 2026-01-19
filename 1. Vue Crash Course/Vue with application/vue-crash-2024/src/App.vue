38a35317f11b6df2f9a5fa6d2bc85a5bc00e0282
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2026-01-19 18:24:36 +1000

    0. Vue - Add form to add/delete tasks on click

--- 1. Vue Crash Course/Vue with application/vue-crash-2024/src/App.vue
+++ 1. Vue Crash Course/Vue with application/vue-crash-2024/src/App.vue
@@ -4,6 +4,7 @@ import {ref} from 'vue';
 const name = ref('John Doe');
 const status = ref('active');
 const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+const newTask = ref('');
 
 const toggleStatus = () => {
   if (status.value === 'active'){
@@ -14,6 +15,18 @@ const toggleStatus = () => {
     status.value = 'active';
   }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() != '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
 </script>
 
 <template>
@@ -25,9 +38,19 @@ const toggleStatus = () => {
   <p v-else="status">User is inactive</p>
 
   <br/>
-  <h3>Tasks: </h3> 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task </label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask"></input>
+    <button type="submit">Submit</button>
+  </form>
+
+  <br/>
+  <h3>Tasks: </h3> <br/>
   <ul>
-    <li v-for="task in tasks" :key="task">{{task}}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span> {{task}} </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
   
   <br/>
