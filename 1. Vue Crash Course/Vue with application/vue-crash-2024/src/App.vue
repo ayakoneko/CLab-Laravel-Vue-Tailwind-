2adb28dee21a283ca7d711ae45bd5ddfda687689
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2026-01-21 09:40:18 +1000

    0. Vue - HomeCard with dynamic bg color setting

--- 1. Vue Crash Course/Vue with application/vue-crash-2024/src/App.vue
+++ 1. Vue Crash Course/Vue with application/vue-crash-2024/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
   import Navbar from '@/components/Navbar.vue';
   import Hero from '@/components/Hero.vue';
+  import HomeCard from '@/components/HomeCard.vue';
 
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCard />
 
 </template>
 
